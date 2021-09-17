19d14a18efe75ffa8d9feccdcf0f521defbdcc74
dhruvinparikh <imsilent93@gmail.com>
2021-09-16 21:47:19 -0400

    class 4 demo

---
+++ 4-class/examples/greeter-full-stack-dapp/hardhat.config.ts
@@ -0,0 +1,44 @@
+import { HardhatUserConfig } from "hardhat/config";
+import { task } from "hardhat/config";
+import { config as dotenvConfig } from "dotenv";
+import "@nomiclabs/hardhat-waffle";
+import { resolve } from "path";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") })
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "rinkeby",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
+
+export default config;
